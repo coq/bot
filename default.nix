7ef1458aa5bb131f1069d6c96570c6b493388cc6
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2018-05-03 14:58:01 +0200

    Stripping out unneeded stuff.

--- default.nix
+++ default.nix
@@ -2,7 +2,6 @@ with import <nixpkgs> {};
 
 let
   ocamlVersion = (builtins.parseDrvName ocamlPackages.ocaml.name).version;
-  merlinWithEmacsMode = ocamlPackages.merlin.override { withEmacsMode = true; };
   findlibSiteLib = "${ocamlPackages.findlib}/lib/ocaml/${ocamlVersion}/site-lib";
 in
 stdenv.mkDerivation rec {
@@ -10,11 +9,10 @@ stdenv.mkDerivation rec {
   src = null;
   buildInputs = with ocamlPackages;
     [ ocaml
-      ocamlbuild
       findlib
-      merlinWithEmacsMode
-      cohttp cohttp-lwt-unix yojson
+      merlin
+      cohttp
+      cohttp-lwt-unix
+      yojson
     ];
-  # For my Emacs config...
-  MERLIN_SITE_LISP = "${merlinWithEmacsMode}/share/emacs/site-lisp";
 }
