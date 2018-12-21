7eb58f1f53ed2b058531aad47383228f01ae5bb6
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2018-12-21 11:46:59 +0100

    Do merge in fresh worktree
    This should ensure fresh state better.
    
    I split the merge command to a new sh file as doing concatenations in
    ocaml is a pain and stops me from using shell linters etc.

---
+++ make_ancestor.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# usage: make_ancestor.sh base ref
+# PWD must be in the relevant git repo
+# merges base in ref if necessary
+
+set -ex
+
+if [ $# != 2 ]; then >&2 echo Bad argument count; fi
+
+base=$1
+ref=$2
+
+basecommit=$(git rev-parse "$base")
+refcommit=$(git rev-parse "$ref")
+
+wtree=$(mktemp -d)
+
+( git checkout --detach HEAD # if ref is already checked out add worktree errors
+  git worktree add "$wtree" "$ref"
+  pushd "$wtree"
+  if ! git merge "$base" -m "Bot merge $basecommit into $refcommit";
+  then
+      popd
+      git worktree remove --force "$wtree"
+      false
+  fi
+)
+git worktree remove --force "$wtree"
