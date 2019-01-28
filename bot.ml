coq/bot

commit ae2419eb1296d10e99b104326b0af580a456bcc7

Fix link to stdlib artifact: the context needs to be different even if the job name is the same.

bot.ml
@@ -527,19 +527,20 @@ let job_action json =
   in
   let send_url (kind, url) =
     (fun () ->
+      let context = Printf.sprintf "%s: %s artifact" build_name kind in
       let description_base = Printf.sprintf "Link to %s build artifact" kind in
       url |> Uri.of_string |> Client.get
       >>= fun (resp, _) ->
       if resp |> Response.status |> Code.code_of_status |> Int.equal 200 then
         send_status_check ~repo_full_name ~commit ~state:"success" ~url
-          ~context:build_name ~description:(description_base ^ ".")
+          ~context ~description:(description_base ^ ".")
       else (
         print_endline "But we didn't get a 200 code when checking the URL." ;
         send_status_check ~repo_full_name ~commit ~state:"failure"
           ~url:
             (Printf.sprintf "https://gitlab.com/%s/-/jobs/%d" repo_full_name
                build_id)
-          ~context:build_name
+          ~context
           ~description:(description_base ^ ": not found.") ) )
     |> Lwt.async
   in

commit 9b8984ea198971a89a893d2193d745b05869e188

Put back doc:refman CI status to green in case the job was retried.
This was broken by ae2419eb1296d10e99b104326b0af580a456bcc7.

bot.ml
@@ -589,31 +589,7 @@ let job_action json =
         print_endline "Unusual error." ;
         send_status_check () ) )
     |> Lwt.async )
-  else if
-    String.equal build_status "success" && String.equal build_name "doc:refman"
-  then (
-    print_endline
-      "This is a successful refman build. Pushing a status check with a link..." ;
-    let url_base =
-      Printf.sprintf
-        "https://coq.gitlab.io/-/coq/-/jobs/%d/artifacts/_install_ci/share/doc/coq"
-        build_id
-    in
-    [ ("refman", Printf.sprintf "%s/sphinx/html/index.html" url_base)
-    ; ("stdlib", Printf.sprintf "%s/html/stdlib/index.html" url_base) ]
-    |> List.iter ~f:send_url )
-  else if
-    String.equal build_status "success"
-    && String.equal build_name "doc:ml-api:odoc"
-  then (
-    print_endline
-      "This is a successful ml-api build. Pushing a status check with a link..." ;
-    ( "ml-api"
-    , Printf.sprintf
-        "https://coq.gitlab.io/-/coq/-/jobs/%d/artifacts/_build/default/_doc/_html/index.html"
-        build_id )
-    |> send_url )
-  else if String.equal build_status "success" then
+  else if String.equal build_status "success" then (
     (fun () ->
       get_status_check ~repo_full_name ~commit ~build_name
       >>= fun b ->
@@ -628,7 +604,28 @@ let job_action json =
           ~context:build_name
           ~description:"Test succeeded on GitLab CI after being retried" )
       else return () )
-    |> Lwt.async
+    |> Lwt.async ;
+    if String.equal build_name "doc:refman" then (
+      print_endline
+        "This is a successful refman build. Pushing a status check with a \
+         link..." ;
+      let url_base =
+        Printf.sprintf
+          "https://coq.gitlab.io/-/coq/-/jobs/%d/artifacts/_install_ci/share/doc/coq"
+          build_id
+      in
+      [ ("refman", Printf.sprintf "%s/sphinx/html/index.html" url_base)
+      ; ("stdlib", Printf.sprintf "%s/html/stdlib/index.html" url_base) ]
+      |> List.iter ~f:send_url )
+    else if String.equal build_name "doc:ml-api:odoc" then (
+      print_endline
+        "This is a successful ml-api build. Pushing a status check with a \
+         link..." ;
+      ( "ml-api"
+      , Printf.sprintf
+          "https://coq.gitlab.io/-/coq/-/jobs/%d/artifacts/_build/default/_doc/_html/index.html"
+          build_id )
+      |> send_url ) )
 
 let pipeline_action json =
   let open Yojson.Basic.Util in