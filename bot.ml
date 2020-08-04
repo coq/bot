9308e70bfec57c924f5023e3ebbd8013e09618f3
Julien Coolen <jtcoolen@pm.me>
2020-08-04 18:30:33 +0200

    Don't check if review comments are created by email.

--- bot.ml
+++ bot.ml
@@ -843,7 +843,10 @@ let callback _conn req body =
                             List.find reviews_info.last_comments ~f:(fun c ->
                                 String.equal comment_info.id c.id)
                           in
-                          if Option.is_none comment then
+                          if
+                            (not comment_info.review_comment)
+                            && Option.is_none comment
+                          then
                             GitHub_mutations.post_comment ~bot_info
                               ~message:
                                 (f
@@ -852,7 +855,8 @@ let callback _conn req body =
                                    comment_info.author)
                               ~id:pr.id
                           else if
-                            (Option.value_exn comment).created_by_email
+                            (not comment_info.review_comment)
+                            && (Option.value_exn comment).created_by_email
                             (* Option.value_exn doesn't raise an exception because comment isn't None at this point*)
                           then
                             GitHub_mutations.post_comment ~bot_info
