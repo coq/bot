6db69f6b9c47f5462b9fcc025dc4424a32663a84
Jason Gross <jgross@mit.edu>
2024-01-27 15:28:25 -0800

    Fix support for url-based resumption via github api

--- bot-components/GitHub_queries.ml
+++ bot-components/GitHub_queries.ml
@@ -1034,3 +1034,9 @@ let get_pull_request_labels ~bot_info ~owner ~repo ~pr_number =
         Lwt.return @@ Error err
   in
   get_list getter
+
+let get_artifact_blob ~bot_info ~owner ~repo ~artifact_id =
+  generic_get_zip ~bot_info
+    (f "repos/%s/%s/actions/artifacts/%s/zip" owner repo artifact_id)
+    (let open Zip in
+     List.map ~f:(fun (entry, contents) -> (entry.filename, contents)) )
--- bot-components/Utils.mli
+++ bot-components/Utils.mli
@@ -17,6 +17,8 @@ val project_api_preview_header : (string * string) list
 
 val app_api_preview_header : (string * string) list
 
+val api_json_header : (string * string) list
+
 val github_header : Bot_info.t -> (string * string) list
 
 val generic_get :
@@ -24,4 +26,11 @@ val generic_get :
   -> string
   -> ?header_list:(string * string) list
   -> (Yojson.Basic.t -> 'a)
+  -> ('a, string) result Lwt.t
+
+val generic_get_zip :
+     bot_info:Bot_info.t
+  -> string
+  -> ?header_list:(string * string) list
+  -> ((Zip.entry * string) list -> 'a)
   -> ('a, string) result Lwt.t
